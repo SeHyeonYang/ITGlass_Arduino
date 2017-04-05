9f7523b66fbfdcd945558e2b954dd722bbf86bfa
SeHyeon Yang <SeHyeon Yang>
2017-04-05 20:50:14 +0900

    where are my commits..?

--- it_glass/it_glass.ino
+++ it_glass/it_glass.ino
@@ -1,185 +1,222 @@
 #include "I2Cdev.h"
-#include "MPU6050.h"
-#include <SoftwareSerial.h>
- 
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE    //defined in "I2Cdev.h"
-#include "Wire.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
 #endif
- 
+#include <SoftwareSerial.h>
 //-------------------------------------HC-06 bluetooth setting.
 int blueTx = 0; // tx=0
 int blueRx = 1; // rx=1
 SoftwareSerial btSerial(blueTx, blueRx);  // btSerial. use to bluetooth communication
 String fromUser=""; // message fromg user
-
+ 
 //------------------------------------RGB led pin number
 int redPin = 4;
 int greenPin = 3;
 int bluePin = 5;
  
-#define XMT   Serial
-//#define XMT   softSerial
+MPU6050 mpu;
+#define OUTPUT_READABLE_YAWPITCHROLL
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
  
-//-------------------------------------MPU6050
-MPU6050 accelgyro;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
  
-#define MPU6050_THRESHOLD (500.0) //(1000.0)
-#define MPU6050_DETECTION_SPEED  (1000/30)   //30hz
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
  
-long MPU6050Timer;
-byte MPU6050ThresholdCap;
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+float checkY[100];
+float checkZ[100];
+#define TEMP 20
+int i=0;
  
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
  
-int16_t pre_ax, pre_ay, pre_az;
-int16_t event_ax, event_ay, event_az;
-int16_t event_gx, event_gy, event_gz;
  
-int16_t pipe[4][6];   //4tab filter
-byte pipeIx;
  
-//-------------------------------------Detection
-long detectionTimer;
-byte noMotionCnt;
-byte keepSendCnt;
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
  
-#define MOTION_DETECTION_HZ    (10) 
-#define MOTION_DETECTION_SPEED  (1000/MOTION_DETECTION_HZ)
-#define KEEP_SEND_TIMES         (10)    //event  10 .
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
  
-  /*---------------------------------------------------------
-      S  E  T  U  P
-  ---------------------------------------------------------*/
+ 
+ 
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+ 
 void setup() {
-  Serial.begin(9600);
-
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+   i=0;
   // bluetooth serial begin
   btSerial.begin(9600);
-
+ 
   // led pin setting
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
-  
-  MPU6050Timer = 0; //long
-  detectionTimer = 0; //long
-  MPU6050ThresholdCap = 0; //byte
-  noMotionCnt = MOTION_DETECTION_HZ; //byte = 10
-  pipeIx = 0;
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
  
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-  Wire.begin();
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-  Fastwire::setup(400, true);
-#endif
-  Serial.println("Initializing I2C devices...");
  
-  accelgyro.initialize();
-  Serial.println("Testing device connections...");
-  Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+ 
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+ 
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+ 
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+ 
+    // supply your own gyro offsets here, scaled for min sensitivity
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+ 
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+ 
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+ 
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+ 
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+ 
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
 }
  
-  /*---------------------------------------------------------
-      L  O  O  P
-  ---------------------------------------------------------*/
+ 
+ 
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+ 
 void loop() {
-  getMessageAndAct();
-  getAccel();
-  getMotion();
-}
+ 
+    if (!dmpReady) return;
+ 
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+    }
+ 
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+ 
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+ 
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+ 
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+ 
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+ 
+        fifoCount -= packetSize;
  
  
-//-----------------------------------------------
-void getMotion(void){
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            checkY[i] = ypr[1] * 180/M_PI;
+            checkZ[i] = ypr[2] * 180/M_PI;
+            //기울기 : 루트 [Y값제곱 x Z값제곱]
+            int Gradient = sqrt( abs(checkY[i] - checkY[abs(i-TEMP)])*abs(checkY[i] - checkY[abs(i-TEMP)])+abs(checkZ[i] - checkZ[abs(i-TEMP)])*abs(checkZ[i] - checkZ[abs(i-TEMP)]));
+            
+           if(Gradient>28)
+           {      
+                 Serial.print("Gradient ");
+                 if(Serial.available()){
+                      btSerial.write(Serial.read());
+                      btSerial.flush();
+                 }
+           }
+//            if(abs(checkY[i] - checkY[abs(i-30)])>28) 
+//                    Serial.print("GradientY ");
+//            if(abs(checkZ[i] - checkZ[abs(i-30)])>28) 
+//                    Serial.print("GradientZ ");
+//            
+            Serial.print("\t");
+            //Y값
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            //Z값
+            Serial.println(ypr[2] * 180/M_PI);
+             
+            i++;
+            if(i==100)
+              i=0;
+           
+        #endif
  
-  long t = millis();
-  if((t- detectionTimer) < MOTION_DETECTION_SPEED) return;
-  detectionTimer = t;
- 
-  if(MPU6050ThresholdCap){    // 6050 event ߻ .
-    sendMotion();
-    keepSendCnt = KEEP_SEND_TIMES;
-    MPU6050ThresholdCap = 0;
-  }
-  else{
-    if(noMotionCnt) noMotionCnt--;
-    if(!noMotionCnt && keepSendCnt){
-      keepSendCnt--;
-      noMotionCnt = MOTION_DETECTION_HZ;  //motion
-    }  
-  }
-}  
  
  
-void sendMotion(void){
-    XMT.println("컵이 움직인다.");
-      if(XMT.available()){
-      btSerial.write(XMT.read());
-      btSerial.flush();
+ 
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
     }
 }
- 
-//-----------------------------------------------
-void getAccel(void){
-  float SVM;
-  float x,y,z;
- 
-  long t = millis();
-  if((t- MPU6050Timer) < MPU6050_DETECTION_SPEED) return;
-  MPU6050Timer = t;
- 
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  pipe[pipeIx][0] = ax;
-  pipe[pipeIx][1] = ay;
-  pipe[pipeIx][2] = az;
-  pipe[pipeIx][3] = gx;
-  pipe[pipeIx][4] = gy;
-  pipe[pipeIx][5] = gz;
-  pipeIx++;
-  pipeIx &= 0x03; //4tab filter
- 
-  ax = tab4(0);
-  ay = tab4(1);
-  az = tab4(2);  
-  gx = tab4(3);
-  gy = tab4(4);
-  gz = tab4(5);  
- 
-  x = pre_ax - ax;
-  y = pre_ay - ay;
-  z = pre_az - az;
- 
-  SVM = sqrt( x*x + y*y + z*z );  
-  pre_ax = ax;
-  pre_ay = ay;
-  pre_az = az;
- 
-  if(SVM >= MPU6050_THRESHOLD){
-    event_ax = ax;
-    event_ay = ay;
-    event_az = az;
- 
-    accelgyro.getRotation(&gx, &gy, &gz);
-    event_gx = gx;
-    event_gy = gy;
-    event_gz = gz;
- 
-    MPU6050ThresholdCap++;
-  }
-}  
- 
-//--------------------
-int tab4(byte i){
-  long t;
-  t = pipe[0][i];
-  t += pipe[1][i];
-  t += pipe[2][i];
-  t += pipe[3][i];  
-  t /= 4;
-  return (int)t;
-}
-
 // RGB 값을 받아 analogWrite를 통해 각 핀에 연결된 LED에 전달
 void setColor(int red, int green, int blue)
 {
@@ -187,9 +224,9 @@ void setColor(int red, int green, int blue)
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue); 
 }
-
+ 
 void getMessageAndAct(){
-
+ 
   // 들어온 메시지가 있으면 받는다.
   while(btSerial.available())
   {
@@ -197,32 +234,24 @@ void getMessageAndAct(){
     fromUser+=myGetChar;
     delay(5);
   } 
-
+ 
   // 메시지에 따른 액션 
   if(!fromUser.equals("")){
-
+ 
     // 앱으로부터 스트링 r.g.b 를 각각 int 수치로 얻는다. 
     char * color = fromUser.c_str();
     char * first = strtok(color, ".");
     char * second = strtok(NULL, ".");
     char * third = strtok(NULL, ".");
-
+ 
     int r = atoi(first);
     int g = atoi(second);
     int b = atoi(third);
-
+ 
     setColor(r,g,b);
-
+ 
     delay(2000);
  
     fromUser="";
   }
   }
-
-  /*
-   Serial.println(fromUser);
-    if(Serial.available()){
-      btSerial.write(Serial.read());
-      btSerial.flush();
-    }
-  */
